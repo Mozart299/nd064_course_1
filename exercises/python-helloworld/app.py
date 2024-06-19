9340fe36b5906f031072628014bd3a0feb64d646
Peter Kyeyune <peterkayk@gmail.com>
2024-06-19 15:56:47 +0300

    chore: Add logging and new endpoints for status and metrics

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify
+import logging
+from datetime import datetime
+
 app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.DEBUG,
+                    format='%(asctime)s, %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 @app.route("/")
 def hello():
+    logging.debug("/hello endpoint was reached")
     return "Hello World!"
+
+@app.route("/status")
+def status():
+    logging.debug("/status endpoint was reached")
+    response = {
+        "result": "OK - healthy",
+        "user": "admin"
+    }
+    return jsonify(response)
+
+@app.route("/metrics")
+def metrics():
+    logging.debug("/metrics endpoint was reached")
+    response = {
+        "data": {
+            "UserCount": 140,
+            "UserCountActive": 23
+        },
+        "user" : "admin"
+    }
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
